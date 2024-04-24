d03f9ab16505a1b66c2f2ab1036a88a68ee93c2b
Raxabi <92801687+Raxabi@users.noreply.github.com>
2024-04-24 18:41:00 +0200

    [ADD] now 'src/code' is ignored by vite bundle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: { // https://github.com/withastro/astro/issues/9859#issuecomment-1936746803
+    optimizeDeps: {
+      exclude: ["@laniakeajs/halley.http"]
+    }
+  }
 });
